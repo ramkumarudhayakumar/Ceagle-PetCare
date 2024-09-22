0a4a89edb300fda6fafd89fdb47bf541e11e8ae4
ramkumarudhayakumar <gymaddicts214@gmail.com>
2024-09-22 23:33:30 +0530

    update commit

--- Servicepage.js
+++ Servicepage.js
@@ -3,3 +3,14 @@ var head = document.querySelector("header");
 bar.addEventListener("click", () => {
   head.classList.toggle("navactive");
 });
+function showContent(index) {
+  const contents = document.querySelectorAll(".content");
+  contents.forEach((content) => {
+    content.style.display = "none";
+  });
+
+  const selectedContent = document.getElementById(`content-${index}`);
+  if (selectedContent) {
+    selectedContent.style.display = "flex";
+  }
+}
